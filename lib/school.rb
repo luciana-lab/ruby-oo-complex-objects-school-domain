77c16e4b31cb18208714b646a1f7be6975d4955c
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-10 14:25:21 -0500

    commit tree 2f9a27385e811f061c38ca5a3234d3c26cf8c741

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+require 'pry'
+#School
+    #::new
+        #has an empty roster when initialized
+    #add_student
+        #is able to add a student
+        #is able to add multiple students to a class (grade)
+        #is able to add students to different grades
+    #grade
+        #is able to retreive students from a grade
+    #sort
+        #is able to sort the students
+class School
+
+    def initialize(school)
+        @roster = {}
+        @school = school
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(students, grades)
+        if @roster.include?(grades)
+            @roster[grades] << students
+        else
+            @roster[grades] = [students]
+        end
+        end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+    end
+end
